fd7a97a8b7b091cd03e400f1b313e14ddb9c7924
Ardika Bagus <me@ardikabs.com>
2020-08-28 02:11:56 +0700

    Update strongswan to be download cert at the runtime

--- Dockerfiles/strongswan/entrypoint.sh
+++ Dockerfiles/strongswan/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-/usr/sbin/charon-cmd --identity $VPN_USER --host $VPN_HOST "$@"
+curl -sSfL https://letsencrypt.org/certs/isrgrootx1.pem.txt -o /tmp/isrgrootx1.pem.txt
+curl -sSfL https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt -o /tmp/letsencryptauthorityx3.pem.txt
+
+/usr/sbin/charon-cmd \
+    --identity $VPN_USER \
+    --host $VPN_HOST \
+    --cert /tmp/isrgrootx1.pem.txt \
+    --cert /tmp/letsencryptauthorityx3.pem.txt \
+    "$@"
