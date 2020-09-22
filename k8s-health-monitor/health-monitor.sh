5bed03a7156a76105226c51958bc4c797d37d8bc
Ardika Bagus <me@ardikabs.com>
2020-09-22 14:14:57 +0700

    Add restart counter both for kubelet and docker

--- k8s-health-monitor/health-monitor.sh
+++ k8s-health-monitor/health-monitor.sh
@@ -2,6 +2,26 @@
 set -o nounset
 set -o pipefail
 
+KUBELET_RESTART=/tmp/kubelet-restart-count
+DOCKER_RESTART=/tmp/docker-restart-count
+
+[[ ! -e ${KUBELET_RESTART} ]] && echo "0" > ${KUBELET_RESTART}
+[[ ! -e ${DOCKER_RESTART} ]] && echo "0" > ${DOCKER_RESTART}
+
+function kubelet_restart(){
+  echo -e "Reason: $1"
+  echo "Kubelet is unhealthy!"
+  echo "Killing kubelet..."
+  pkill --signal 9 kubelet
+  sleep 5
+  echo "Re-starting kubelet.."
+  systemctl start kubelet
+  echo "$((`cat ${KUBELET_RESTART}` + 1))" > ${KUBELET_RESTART}
+  echo -e "Kubelet restart count: `cat ${KUBELET_RESTART}`"
+
+  sleep 120
+}
+
 function docker_monitoring {
   while [ 1 ]; do
     if ! timeout 60 docker ps > /dev/null; then
@@ -10,6 +30,9 @@ function docker_monitoring {
       # Wait for a while, as we don't want to kill it again before it is really up.
       sleep 120
       systemctl start docker
+      echo "$((`cat ${DOCKER_RESTART}` + 1))" > ${DOCKER_RESTART}
+      echo -e "Docker restart count: `cat ${DOCKER_RESTART}`"
+
     else
       sleep "${SLEEP_SECONDS}"
     fi
@@ -23,23 +46,11 @@ function kubelet_monitoring {
   local output=""
   while [ 1 ]; do
     if ! output=$(curl -m "${max_seconds}" -f -s -S http://127.0.0.1:10255/healthz 2>&1); then
-      # Print the response and/or errors.
-      echo $output
-      echo "Kubelet is unhealthy!"
-      echo "Killing kubelet..."
-      pkill --signal 9 kubelet
-      sleep 5
-      echo "Re-starting kubelet.."
-      systemctl start kubelet
-      sleep 120
+      kubelet_restart $output
+
     elif ! output=$(journalctl -u kubelet -n50 | grep -m1 -o "use of closed network connection" 2>&1); then
-      echo "Kubelet caught error for 'use of closed network connection'"
-      echo "Killing kubelet ..."
-      pkill --signal 9 kubelet
-      sleep 5
-      echo "Re-starting kubelet.."
-      systemctl start kubelet
-      sleep 120
+      kubelet_restart "Kubelet has caught 'use of closed network connection'"
+
     else
       sleep "${SLEEP_SECONDS}"
     fi
