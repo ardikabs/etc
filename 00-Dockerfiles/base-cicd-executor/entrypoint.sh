a30f1d596a793e1c8c13a6bc639d28b0bbad9b7d
Ardika Bagus <me@ardikabs.com>
2020-12-02 23:51:44 +0700

    cicd | Update entrypoint script

--- 00-Dockerfiles/base-cicd-executor/entrypoint.sh
+++ 00-Dockerfiles/base-cicd-executor/entrypoint.sh
@@ -44,4 +44,19 @@ mv "${TEMPDIR}"/shared /opt/shared || true
 
 rm -rf "${TEMPDIR}"
 
-exec "$@"
+if [ -x /usr/local/bin/bash ]; then
+    exec /usr/local/bin/bash "$@"
+elif [ -x /usr/bin/bash ]; then
+    exec /usr/bin/bash "$@"
+elif [ -x /bin/bash ]; then
+    exec /bin/bash "$@"
+elif [ -x /usr/local/bin/sh ]; then
+    exec /usr/local/bin/sh "$@"
+elif [ -x /usr/bin/sh ]; then
+    exec /usr/bin/sh "$@"
+elif [ -x /bin/sh ]; then
+    exec /bin/sh "$@"
+else
+    echo shell not found
+    exit 1
+fi
