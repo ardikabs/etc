8e59038098ecefa00f81ba8efdce310d80b099fb
Ardika Bagus <me@ardikabs.com>
2021-02-17 13:43:56 +0700

    Fix more controllable manner on kink

--- dockerfiles/kink/entrypoint.sh
+++ dockerfiles/kink/entrypoint.sh
@@ -3,32 +3,63 @@
 set -o errexit
 set -o nounset
 
-if [ "${KINK_API_SERVER:-"x"}" = x ]; then
-  K3S_ARGS=""
+KINK_ARGS=""
 
-  if [ -n "${KINK_MASTER_SAN:-}" ]; then
-    KINK_MASTER_SAN=$(echo "${KINK_MASTER_SAN}" | tr "," " ")
-    for san in ${KINK_MASTER_SAN}; do
-      K3S_ARGS="${K3S_ARGS} --tls-san ${san}"
-    done
-  fi
-
-  set -- k3s server \
-    --no-deploy=servicelb \
-    --no-deploy=traefik \
-    --https-listen-port=443 \
-    --token="${KINK_TOKEN:-"kink-k3s"}" \
-    --node-taint master=true:NoExecute \
-    "${K3S_ARGS}"
-else
-  echo "wait for api server to up..."
-  until wget -q --spider http://"${KINK_API_SERVER}":10251/healthz 2>/dev/null; do
-    echo "."
-    sleep 1
+parseArgs() {
+  for san in $(echo "${KINK_APISERVER_SAN}" | tr ',' ' '); do
+    KINK_ARGS="${KINK_ARGS} --tls-san ${san}"
   done
-  echo "done"
 
-  set -- k3s agent --server=https://"${KINK_API_SERVER}" --token="${KINK_TOKEN:-"kink-k3s"}"
-fi
+  for ip in $(echo "${KINK_EXTERNAL_IPS:-}" | tr ',' ' '); do
+    KINK_ARGS="${KINK_ARGS} --node-external-ip ${ip}"
+  done
 
-exec "$@"
+  if [ -n "${KINK_EXTERNAL_IP:-}" ]; then KINK_ARGS="${KINK_ARGS} --node-external-ip ${KINK_EXTERNAL_IP}"; fi
+
+  if [ -n "${KINK_APISERVER:-}" ]; then KINK_ARGS="${KINK_ARGS} --server https://${KINK_APISERVER}:${KINK_APISERVER_PORT:-443}"; fi
+
+  KINK_ARGS="${KINK_ARGS} --token ${KINK_TOKEN:-"k3s-kink"}"
+}
+
+main() {
+  opt=${1:-''}
+
+  case $opt in
+    server)
+      shift 1
+      parseArgs
+      KINK_ARGS="${KINK_ARGS} --no-deploy=servicelb"
+      KINK_ARGS="${KINK_ARGS} --no-deploy=traefik"
+      KINK_ARGS="${KINK_ARGS} --https-listen-port=443"
+      KINK_ARGS="${KINK_ARGS} --node-taint master=true:NoExecute"
+      KINK_ARGS="${KINK_ARGS} --token ${KINK_TOKEN:-"k3s-kink"}"
+      KINK_ARGS="${KINK_ARGS} $*"
+      shift $#
+
+      eval "set -- k3s server ${KINK_ARGS}"
+    ;;
+    agent)
+      shift 1
+      [ -n "${KINK_APISERVER:-}" ] || {
+          echo "\$KINK_APISERVER required, exiting..." >&2
+          exit 1
+      }
+      parseArgs
+      KINK_ARGS="${KINK_ARGS} $*"
+      shift $#
+
+      echo "wait for api server to up..."
+      until wget -q --spider http://"${KINK_APISERVER}":10251/healthz 2>/dev/null; do
+        echo "."
+        sleep 1
+      done
+
+      eval "set -- k3s agent ${KINK_ARGS}"
+    ;;
+  esac
+
+  printf "Running the following command: \n %s \n\n" "$*"
+  exec "$@"
+}
+
+main "$@"
