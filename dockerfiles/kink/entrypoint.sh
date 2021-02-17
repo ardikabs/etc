89a077a6dc345b8c117df672ba0f9462f91e0815
Ardika Bagus <me@ardikabs.com>
2021-02-17 16:17:27 +0700

    Add support to resolve external address

--- dockerfiles/kink/entrypoint.sh
+++ dockerfiles/kink/entrypoint.sh
@@ -5,19 +5,40 @@ set -o nounset
 
 KINK_ARGS=""
 
-parseArgs() {
-  for san in $(echo "${KINK_APISERVER_SAN}" | tr ',' ' '); do
-    KINK_ARGS="${KINK_ARGS} --tls-san ${san}"
-  done
+err() {
+  echo "$*" >&2
+  exit 1
+}
 
-  for ip in $(echo "${KINK_EXTERNAL_IPS:-}" | tr ',' ' '); do
-    KINK_ARGS="${KINK_ARGS} --node-external-ip ${ip}"
-  done
+resolve() {
+  nslookup "$1" | awk '/^Address: / { print $2 }'
+}
 
+parse_env() {
+  # Resolving external address to IPs
+  # also add external address to API Server SAN
+  if [ -n "${KINK_EXTERNAL_ADDRESS:-}" ]; then
+    until [ -n "$(resolve "${KINK_EXTERNAL_ADDRESS}")" ]; do
+      echo "waiting address ${KINK_EXTERNAL_ADDRESS} to resolve ..."
+      sleep 1
+    done
+
+    result=$(resolve "${KINK_EXTERNAL_ADDRESS}" | tr '\n' ',')
+
+    KINK_APISERVER_SAN="${KINK_APISERVER_SAN:-},${KINK_EXTERNAL_ADDRESS}"
+    KINK_EXTERNAL_IPS="${KINK_EXTERNAL_IPS:-},${result}"
+  fi
+
+  # Parsing env for single value flag
+  if [ -n "${KINK_APISERVER:-}" ]; then KINK_ARGS="${KINK_ARGS} --server https://${KINK_APISERVER}:${KINK_APISERVER_PORT:-443}"; fi
   if [ -n "${KINK_EXTERNAL_IP:-}" ]; then KINK_ARGS="${KINK_ARGS} --node-external-ip ${KINK_EXTERNAL_IP}"; fi
 
-  if [ -n "${KINK_APISERVER:-}" ]; then KINK_ARGS="${KINK_ARGS} --server https://${KINK_APISERVER}:${KINK_APISERVER_PORT:-443}"; fi
+  # Parsing env for multiple values flag,
+  # based on separated by comma values
+  for san in $(echo "${KINK_APISERVER_SAN:-}" | tr ',' ' '); do KINK_ARGS="${KINK_ARGS} --tls-san ${san}"; done
+  for ip in $(echo "${KINK_EXTERNAL_IPS:-}" | tr ',' ' '); do KINK_ARGS="${KINK_ARGS} --node-external-ip ${ip}"; done
 
+  # Default k3s token
   KINK_ARGS="${KINK_ARGS} --token ${KINK_TOKEN:-"k3s-kink"}"
 }
 
@@ -27,7 +48,7 @@ main() {
   case $opt in
     server)
       shift 1
-      parseArgs
+      parse_env
       KINK_ARGS="${KINK_ARGS} --no-deploy=servicelb"
       KINK_ARGS="${KINK_ARGS} --no-deploy=traefik"
       KINK_ARGS="${KINK_ARGS} --https-listen-port=443"
@@ -40,17 +61,14 @@ main() {
     ;;
     agent)
       shift 1
-      [ -n "${KINK_APISERVER:-}" ] || {
-          echo "\$KINK_APISERVER required, exiting..." >&2
-          exit 1
-      }
-      parseArgs
+      [ -n "${KINK_APISERVER:-}" ] || err "\$KINK_APISERVER required for agent setup, exiting..."
+
+      parse_env
       KINK_ARGS="${KINK_ARGS} $*"
       shift $#
 
-      echo "wait for api server to up..."
       until wget -q --spider http://"${KINK_APISERVER}":10251/healthz 2>/dev/null; do
-        echo "."
+        echo "wait for api server to up..."
         sleep 1
       done
 
