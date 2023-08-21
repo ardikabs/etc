944ccb1fb0cb78b3e763a086c3a497804a03f185
Ardika Bagus <me@ardikabs.com>
2023-08-21 17:53:34 +0700

    feat: add kubexec

--- dockerfiles/kubexec/README.md
+++ dockerfiles/kubexec/README.md
@@ -7,6 +7,7 @@ All-in-one image for Kubernetes related operations. The following tools installe
 * Helm v3.12.0
 * Helmfile v0.154.0
 * Mozilla/SOPS v3.7.3
+* ArgoCD v2.8.0
 
 Also included with the following `helm` plugins:
 
@@ -18,14 +19,17 @@ Also included with the following `helm` plugins:
 You can also able to override version of the pre-installed tools, by triggering the `/docker-entrypoint.sh` followed with environment variable belows:
 
 ```bash
-export HELM_VERSION=v3.12.0
-export KUBECTL_VERSION=v1.27.1
-export KUSTOMIZE_VERSION=v4.4.1
-export SOPS_VERSION=v3.7.3
-export HELMFILE_VERSION=0.154.0
-export HELM_PLUGIN_DIFF_VERSION=3.8.0
-export HELM_PLUGIN_PUSH_VERSION=0.10.3
-export HELM_PLUGIN_UNITTEST_VERSION=0.2.3
+export KUBECTL_VERSION=1.27.1
+export ARGOCD_VERSION=2.8.0
+export KUSTOMIZE_VERSION=5.0.1
+export SOPS_VERSION=3.7.3
+export HELM_VERSION=3.12.0
+export HELMFILE_VERSION=0.156.0
+export HELM_PLUGIN_DIFF_VERSION=3.8.1
+export HELM_PLUGIN_PUSH_VERSION=0.10.4
+export HELM_PLUGIN_UNITTEST_VERSION=0.3.4
+export HELM_CHART_RELEASER_VERSION=1.6.0
+export HELM_CHART_TESTING_VERSION=3.9.0
 ```
 
 ## Status
--- dockerfiles/kubexec/entrypoint.sh
+++ dockerfiles/kubexec/entrypoint.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-wget -q "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" -O - | tar -xz --strip-component=1 --directory=/usr/local/bin linux-amd64/helm
-wget -q "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl" -O /usr/local/bin/kubectl
-wget -q "https://github.com/helmfile/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_linux_amd64.tar.gz" -O - | tar -xz --directory=/usr/local/bin helmfile
-wget -q "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz" -O - | tar -xz --directory=/usr/local/bin kustomize
-wget -q "https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}.linux" -O /usr/local/bin/sops
+install() {
+  local name
+  name=$1
+  version="${!name}"
 
-chmod +x /usr/local/bin/{kubectl,helm,helmfile,kustomize,sops}
+  if grep "$name ${!name}" /usr/share/.installed-version >/dev/null 2>&1; then
+    echo >&2 "$name: the tool already exists, skipping..."
+    return
+  fi
 
-helm plugin install https://github.com/databus23/helm-diff --version "${HELM_PLUGIN_DIFF_VERSION}" >/dev/null 2>&1
-helm plugin install https://github.com/chartmuseum/helm-push --version "${HELM_PLUGIN_PUSH_VERSION}" >/dev/null 2>&1
-helm plugin install https://github.com/quintush/helm-unittest --version=${HELM_PLUGIN_UNITTEST_VERSION} >/dev/null 2>&1
+  echo >&2 "$name: installing"
+  case $name in
+  KUBECTL_VERSION) wget -q "https://storage.googleapis.com/kubernetes-release/release/v${version}/bin/linux/amd64/kubectl" -O /usr/local/bin/kubectl ;;
+  ARGOCD_VERSION) wget -q "https://github.com/argoproj/argo-cd/releases/download/v${version}/argocd-linux-amd64" -O /usr/local/bin/argocd ;;
+  KUSTOMIZE_VERSION) wget -q "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_linux_amd64.tar.gz" -O - | tar -xz --directory=/usr/local/bin kustomize ;;
+  SOPS_VERSION) wget -q "https://github.com/mozilla/sops/releases/download/v${version}/sops-v${version}.linux" -O /usr/local/bin/sops ;;
+  HELM_VERSION) wget -q "https://get.helm.sh/helm-v${version}-linux-amd64.tar.gz" -O - | tar -xz --strip-component=1 --directory=/usr/local/bin linux-amd64/helm ;;
+  HELMFILE_VERSION) wget -q "https://github.com/helmfile/helmfile/releases/download/v${version}/helmfile_${version}_linux_amd64.tar.gz" -O - | tar -xz --directory=/usr/local/bin helmfile ;;
+  HELM_CHART_RELEASER_VERSION) wget -q "https://github.com/helm/chart-releaser/releases/download/v${version}/chart-releaser_${version}_linux_amd64.tar.gz" -O - | tar -xz --directory=/usr/local/bin cr ;;
+  HELM_CHART_TESTING_VERSION) wget -q "https://github.com/helm/chart-testing/releases/download/v${version}/chart-testing_${version}_linux_amd64.tar.gz" -O - | tar -xz --directory=/usr/local/bin ct ;;
+  HELM_PLUGIN_DIFF_VERSION) helm plugin install https://github.com/databus23/helm-diff --version "${version}" >/dev/null 2>&1 ;;
+  HELM_PLUGIN_PUSH_VERSION) helm plugin install https://github.com/chartmuseum/helm-push --version "${version}" >/dev/null 2>&1 ;;
+  HELM_PLUGIN_UNITTEST_VERSION) helm plugin install https://github.com/quintush/helm-unittest --version "${version}" >/dev/null 2>&1 ;;
+  esac
 
+  echo "$name" "${!name}" >>/usr/share/.installed-version
+}
+
+for var in $(env | cut -d '=' -f 1); do
+  # Check if the variable ends with _VERSION
+  if [[ $var == *_VERSION ]]; then
+    install $var
+  fi
+done
+
+chmod +x /usr/local/bin/{kubectl,argocd,kustomize,sops,helm,helmfile,ct,cr} || true
 exec "$@"
